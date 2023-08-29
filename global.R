bc0bcd82340c30e6b50f3e94ec3ffb7a82dc0745
armando-v <>
2023-08-28 23:59:16 -0400

    add shiny libraries

--- global.R
+++ global.R
@@ -2,6 +2,10 @@ library(DBI)
 library(odbc)
 library(tidyverse)
 library(pool)
+library(shiny)
+library(shinydashboardPlus)
+library(shinycssloaders)
+
 
 con <- dbPool(drv = odbc(), dsn = "OAO Cloud DB")
 
